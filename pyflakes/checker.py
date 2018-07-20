acf98b270da0f3f33c03ddcfc272e268dbded49b
John Vandenberg <jayvdb@gmail.com>
2018-07-20 07:33:04 +0700

    Fix doctest error offset for PyPy 3.6

--- pyflakes/checker.py
+++ pyflakes/checker.py
@@ -12,6 +12,7 @@ import sys
 
 PY2 = sys.version_info < (3, 0)
 PY34 = sys.version_info < (3, 5)    # Python 2.7 to 3.4
+PY35 = sys.version_info < (3, 6)    # Python 2.7 to 3.4
 try:
     sys.pypy_version_info
     PYPY = True
@@ -951,7 +952,7 @@ class Checker(object):
                 tree = compile(example.source, "<doctest>", "exec", ast.PyCF_ONLY_AST)
             except SyntaxError:
                 e = sys.exc_info()[1]
-                if PYPY:
+                if PYPY and PY35:
                     e.offset += 1
                 position = (node_lineno + example.lineno + e.lineno,
                             example.indent + 4 + (e.offset or 0))
